60c63a39319b6c8f8edda4ecf7067b8d4f49da5f
patriciaarnedo <13356497+PatriciaArnedo@users.noreply.github.com>
2024-02-15 22:42:45 -0500

    add lazy loading

--- src/Animation.jsx
+++ src/Animation.jsx
@@ -1,10 +1,18 @@
-import Spline from "@splinetool/react-spline";
-import React from "react";
+import React, { Suspense, useState } from "react";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
+import { CircularProgress, Box } from "@mui/material";
 
 export default function Animation() {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <div>
-      <Spline scene="https://prod.spline.design/QZ4ohwGm3LiPT6bL/scene.splinecode" />
-    </div>
+    <Box>
+      <Suspense fallback={<CircularProgress />}>
+        <Spline
+          onLoad={() => setLoaded(true)}
+          scene="https://prod.spline.design/QZ4ohwGm3LiPT6bL/scene.splinecode"
+        />
+      </Suspense>
+    </Box>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
